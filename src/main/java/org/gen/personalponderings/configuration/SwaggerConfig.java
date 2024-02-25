835e3e82e7f07269fd075a506334f1b77a039ff3
johnnyalvesx <alvesjohnny@outlook.com>
2024-02-24 23:39:32 -0300

    📝 Ajuste swagger

--- src/main/java/org/gen/personalponderings/configuration/SwaggerConfig.java
+++ src/main/java/org/gen/personalponderings/configuration/SwaggerConfig.java
@@ -31,21 +31,6 @@ public class SwaggerConfig {
                                 .name("Johnny Alves")
                                 .email("johnnyalves@example.com")
                                 .url("https://github.com/johnnyalvesx")));
-//                .components(createGlobalResponses());
 
     }
-//    private Components createGlobalResponses() {
-//        Components components = new Components();
-//        components.addResponses("200", new io.swagger.v3.oas.models.responses.ApiResponse().description("Sucesso!"));
-//        components.addResponses("201", new io.swagger.v3.oas.models.responses.ApiResponse().description("Criado!"));
-//        components.addResponses("400", new io.swagger.v3.oas.models.responses.ApiResponse().description("Erro na requisição!"));
-//        components.addResponses("401", new io.swagger.v3.oas.models.responses.ApiResponse().description("Não Autorizado!"));
-//        components.addResponses("403", new io.swagger.v3.oas.models.responses.ApiResponse().description("Proibido!"));
-//        components.addResponses("404", new io.swagger.v3.oas.models.responses.ApiResponse().description("Não Encontrado!"));
-//        components.addResponses("500", new io.swagger.v3.oas.models.responses.ApiResponse().description("Erro!"));
-//        return components;
-//    }
-
-
-
 }
